8af3d8c2128eb7cbc897a69f852c47a3e46c6f7b
Thiago Diniz <thiagovsdiniz@gmail.com>
2011-09-09 10:51:35 -0300

    Token generation now using SecureRandom

--- lib/uniquify.rb
+++ lib/uniquify.rb
@@ -6,12 +6,12 @@ module Uniquify
   module ClassMethods
 
     def uniquify(*args, &block)
-      options = { :length => 8, :chars => ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a }
+      options = { :length => 15 }
       options.merge!(args.pop) if args.last.kind_of? Hash
-      
+
       class_inheritable_reader(:uniquify_options)
       write_inheritable_attribute(:uniquify_options, options)
-      
+
       args.each do |name|
         before_validation :on => :create do |record|
           if block
@@ -22,17 +22,17 @@ module Uniquify
         end
       end
     end
-        
+
     def generate_unique
-      Array.new(uniquify_options[:length]) { uniquify_options[:chars].to_a[rand(uniquify_options[:chars].to_a.size)] }.join
+        SecureRandom.base64(uniquify_options[:length]).tr('+/=lIO0', 'pqrsxyz')
     end
   end
-  
+
   def ensure_unique(name, &block)
     begin
       self[name] = (block ? block.call : self.class.generate_unique)
     end while self.class.exists?(name => self[name])
-    
+
     self[name]
   end
 end
